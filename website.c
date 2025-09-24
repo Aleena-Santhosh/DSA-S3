662c17201ef63ebdf20b2589981f96cf37d8f0b3
Aleena-Santhosh <aleenasanthosh949@gmail.com>
2025-09-24 11:45:46 +0530

    Add files via upload

---
+++ website.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node {
+    char url[100];
+    struct Node* prev;
+    struct Node* next;
+};
+
+struct Node* current = NULL; 
+
+struct Node* createNode(char* url) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    strcpy(newNode->url, url);
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void visitPage(char* url) {
+    struct Node* newNode = createNode(url);
+
+    if (current != NULL) {
+
+        current->next = NULL;
+        newNode->prev = current;
+        current->next = newNode;
+    }
+    current = newNode;
+    printf("Visited: %s\n", url);
+}
+
+void goBack() {
+    if (current != NULL && current->prev != NULL) {
+        current = current->prev;
+        printf("Back to: %s\n", current->url);
+    } else {
+        printf("No previous page!\n");
+    }
+}
+
+void goForward() {
+    if (current != NULL && current->next != NULL) {
+        current = current->next;
+        printf("Forward to: %s\n", current->url);
+    } else {
+        printf("No forward page!\n");
+    }
+}
+
+void showCurrent() {
+    if (current != NULL) {
+        printf("Current page: %s\n", current->url);
+    } else {
+        printf("No page loaded!\n");
+    }
+}
+
+int main() {
+    int choice;
+    char url[100];
+
+    printf("Browser Navigation Menu:\n");
+    printf("1. Visit New Page\n");
+    printf("2. Back\n");
+    printf("3. Forward\n");
+    printf("4. Show Current Page\n");
+    printf("5. Exit\n");
+
+    while (1) {
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+        getchar(); 
+
+        switch (choice) {
+            case 1:
+                printf("Enter URL: ");
+                scanf("%s", url);
+                visitPage(url);
+                break;
+            case 2:
+                goBack();
+                break;
+            case 3:
+                goForward();
+                break;
+            case 4:
+                showCurrent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    }
+    return 0;
+}
